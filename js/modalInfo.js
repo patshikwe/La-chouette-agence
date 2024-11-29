ff518576b96fce7d821a6f9a63532c993683cf28
patshikwe <patshikwe@yahoo.fr>
2024-11-29 01:56:46 +0100

    Vérification

--- js/modalInfo.js
+++ js/modalInfo.js
@@ -2,55 +2,55 @@
 function modalInfo() {
 
     // Récupération des éléments HTML
-    const formOne = document.querySelector("#form_1")
-    const modalInfo = document.querySelector("#modalInfo")
-    const btnCancel = document.querySelector("#btnCancel")
-    const name = document.querySelector("#name")
-    const errorName = document.querySelector("#errorName")
-    const email = document.querySelector("#email")
-    const errorEmail = document.querySelector("#errorEmail")
-    const input504 = document.querySelector("#input_504")
-    const errorInput504 = document.querySelector("#errorInput504")
-    const message = document.querySelector("#message")
-    const errorMessage = document.querySelector("#errorMessage")
-    const contact = document.querySelector("#contact")
+    const formOne = document.querySelector("#form_1");
+    const modalInfo = document.querySelector("#modalInfo");
+    const btnCancel = document.querySelector("#btnCancel");
+    const name = document.querySelector("#name");
+    const errorName = document.querySelector("#errorName");
+    const email = document.querySelector("#email");
+    const errorEmail = document.querySelector("#errorEmail");
+    const input504 = document.querySelector("#input_504");
+    const errorInput504 = document.querySelector("#errorInput504");
+    const message = document.querySelector("#message");
+    const errorMessage = document.querySelector("#errorMessage");
+    const contact = document.querySelector("#contact");
 
-    let input504LengthMin = 5
-    let input504Length = 50
+    let input504LengthMin = 5;
+    let input504Length = 50;
 
-    let messageLengthMin = 10
-    let messageLength = 1000
+    let messageLengthMin = 10;
+    let messageLength = 1000;
 
-    let nameCheck = ""
-    let nameTesting = true
+    let nameCheck = "";
+    let nameTesting = true;
 
-    let emailCheck = ""
-    let emailTesting = true
+    let emailCheck = "";
+    let emailTesting = true;
 
-    let input504Check = ""
-    let input504Testing = true
+    let input504Check = "";
+    let input504Testing = true;
 
-    let messageCheck = ""
-    let messageTesting = true
+    let messageCheck = "";
+    let messageTesting = true;
 
     //Regex
-    const nameRegex = /^[a-zA-ZÅåÄàäôÖöØøÆæçÉéÈèùÜüÊêÛûÎî-\s]+$/
-    const emailRegex = /^[a-zA-Z0-9._-]+[@]{1}[a-zA-Z0-9._-]+[.]{1}[a-z]{2,10}$/
-    const Input504Regex = /[a-z]+/g
-    const MessageRegex = /[a-z]+/g
+    const nameRegex = /^[a-zA-ZÅåÄàäôÖöØøÆæçÉéÈèùÜüÊêÛûÎî-\s]+$/;
+    const emailRegex = /^[a-zA-Z0-9._-]+[@]{1}[a-zA-Z0-9._-]+[.]{1}[a-z]{2,10}$/;
+    const Input504Regex = /[a-z]+/g;
+    const MessageRegex = /[a-z]+/g;
 
     // Ecouteur name (nom)
     name.addEventListener('change', () => {
 
-        nameCheck = nameRegex.test($(name).val())
+        nameCheck = nameRegex.test($(name).val());
 
         if (nameCheck === false) {
-            errorName.classList.remove("dspl-none")
-            errorName.style.color = "red"
-            name.classList.add("invalid")
+            errorName.classList.remove("dspl-none");
+            errorName.style.color = "red";
+            name.classList.add("invalid");
         } else {
-            errorName.classList.add("dspl-none")
-            name.classList.remove("invalid")
+            errorName.classList.add("dspl-none");
+            name.classList.remove("invalid");
         }
     })
 
@@ -58,15 +58,15 @@ function modalInfo() {
     // Ecouteur email
     email.addEventListener('change', () => {
 
-        emailCheck = emailRegex.test($(email).val())
+        emailCheck = emailRegex.test($(email).val());
 
         if (emailCheck === false) {
-            errorEmail.classList.remove("dspl-none")
-            errorEmail.style.color = "red"
-            email.classList.add("invalid")
+            errorEmail.classList.remove("dspl-none");
+            errorEmail.style.color = "red";
+            email.classList.add("invalid");
         } else {
-            errorEmail.classList.add("dspl-none")
-            email.classList.remove("invalid")
+            errorEmail.classList.add("dspl-none");
+            email.classList.remove("invalid");
         }
     })
 
@@ -74,16 +74,16 @@ function modalInfo() {
     // Ecouteur input_504
     input504.addEventListener('change', () => {
 
-        input504Check = Input504Regex.test($(input504).val())
+        input504Check = Input504Regex.test($(input504).val());
 
         if (input504Check === false || $(input504).val().length > input504Length
             || $(input504).val().length < input504LengthMin) {
-            errorInput504.classList.remove("dspl-none")
-            errorInput504.style.color = "red"
-            input504.classList.add("invalid")
+            errorInput504.classList.remove("dspl-none");
+            errorInput504.style.color = "red";
+            input504.classList.add("invalid");
         } else {
-            errorInput504.classList.add("dspl-none")
-            input504.classList.remove("invalid")
+            errorInput504.classList.add("dspl-none");
+            input504.classList.remove("invalid");
         }
     })
 
@@ -91,23 +91,23 @@ function modalInfo() {
     // Ecouteur message
     message.addEventListener('change', () => {
 
-        messageCheck = MessageRegex.test($(message).val())
+        messageCheck = MessageRegex.test($(message).val());
 
         if (messageCheck === false || $(message).val().length > messageLength
             || $(message).val().length < messageLengthMin) {
-            errorMessage.classList.remove("dspl-none")
-            errorMessage.style.color = "red"
-            message.classList.add("invalid")
+            errorMessage.classList.remove("dspl-none");
+            errorMessage.style.color = "red";
+            message.classList.add("invalid");
         } else {
-            errorMessage.classList.add("dspl-none")
-            message.classList.remove("invalid")
+            errorMessage.classList.add("dspl-none");
+            message.classList.remove("invalid");
         }
     })
 
 
     //Ecouteur formulaire pour envoyer
     formOne.addEventListener('submit', (e) => {
-        e.preventDefault()
+        e.preventDefault();
 
         if (nameCheck === nameTesting && emailCheck === emailTesting
             && input504Check === input504Testing && messageCheck === messageTesting
@@ -116,28 +116,28 @@ function modalInfo() {
             && $(input504).val().length >= input504LengthMin
             && $(message).val().length >= messageLengthMin) {
 
-            modalInfo.classList.remove("dspl-none")
+            modalInfo.classList.remove("dspl-none");
         }
     })
 
 
     // Fermer la fenêtre modale et vider les inputs
     btnCancel.onclick = () => {
-        modalInfo.classList.add("dspl-none")
-        name.value = ""
-        email.value = ""
-        input504.value = ""
-        message.value = ""
+        modalInfo.classList.add("dspl-none");
+        name.value = "";
+        email.value = "";
+        input504.value = "";
+        message.value = "";
     }
 
 
     // Fermer la fenêtre modale et vider les inputs
     contact.onclick = () => {
-        modalInfo.classList.add("dspl-none")
-        name.value = ""
-        email.value = ""
-        input504.value = ""
-        message.value = ""
+        modalInfo.classList.add("dspl-none");
+        name.value = "";
+        email.value = "";
+        input504.value = "";
+        message.value = "";
     }
 }
-modalInfo()
+modalInfo();
